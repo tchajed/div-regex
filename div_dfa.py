600c91e4ea04c3956c67d172701c352c89500b28
Tej Chajed <tchajed@mit.edu>
2017-04-05 14:02:55 -0400

    Remove DFA printing main function

--- div_dfa.py
+++ div_dfa.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from __future__ import print_function
-
 from dfa import Dfa
 
 def divisible_by(n):
@@ -13,25 +11,3 @@ def divisible_by(n):
             s_delta[x] = (s * 10 + d)%n
         delta.append(s_delta)
     return Dfa(delta, set([0]), 0)
-
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("n", type=int,
-                        help="modulus to test divisibility against")
-
-    args = parser.parse_args()
-
-    dfa = divisible_by(args.n)
-
-    print(" " * 4, end="")
-    for d in range(10):
-        print("{:3}".format(d), end="")
-    print("")
-    for s in range(args.n):
-        print("{:3} ".format(s), end="")
-        for d in range(10):
-            x = str(d)
-            print("{:3}".format(dfa.transition(s, x)), end="")
-        print("")
